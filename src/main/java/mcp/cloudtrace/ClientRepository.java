a2db03014ea70256f1341ceb4c75a2c6f9da3ac7
Mario Gray <mario.gray@gmail.com>
2018-02-27 16:32:47 -0800

    Adds sample data

--- src/main/java/mcp/cloudtrace/ClientRepository.java
+++ src/main/java/mcp/cloudtrace/ClientRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 
+// contrifed repository - probably not needed to illustrate tracing concerns.. maybe.
 interface ClientRepository extends JpaRepository<Client, Long> {
     Collection<Client> findByClientId(String dn);
 }
--- src/main/java/mcp/cloudtrace/CloudTraceApplication.java
+++ src/main/java/mcp/cloudtrace/CloudTraceApplication.java
@@ -3,7 +3,11 @@ package mcp.cloudtrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 
 @SpringBootApplication
@@ -13,6 +17,16 @@ public class CloudTraceApplication {
         return new RestTemplate();
     }
 
+    // SETUP SOME DATA! - this is an example afterall
+    @EventListener
+    public void handleContextBeforeRefresh(ContextRefreshedEvent event) {
+        event.getApplicationContext().getBean(ClientRepository.class)
+                .save(Arrays.asList(
+                   new Client(null, "IOS"),
+                   new Client(null, "ANDROID"),
+                   new Client(null, "WEB")
+                ));
+    }
     public static void main(String[] args) {
         SpringApplication.run(CloudTraceApplication.class, args);
     }
