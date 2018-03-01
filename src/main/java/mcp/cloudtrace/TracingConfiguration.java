a1784c39d1d90520346dbd16d4c0362bd115fe0d
Josh Long <starbuxman@gmail.com>
2018-02-28 16:26:40 -0800

    beans

--- src/main/java/mcp/cloudtrace/TracingConfiguration.java
+++ src/main/java/mcp/cloudtrace/TracingConfiguration.java
@@ -8,13 +8,12 @@ import brave.propagation.ExtraFieldPropagation;
 import brave.sampler.Sampler;
 import brave.spring.web.TracingClientHttpRequestInterceptor;
 import brave.spring.webmvc.TracingHandlerInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import zipkin2.Span;
@@ -22,86 +21,85 @@ import zipkin2.reporter.AsyncReporter;
 import zipkin2.reporter.Sender;
 import zipkin2.reporter.okhttp3.OkHttpSender;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * This adds tracing configuration to any web mvc controllers or rest template clients. This should
  * be configured last.
  */
 @Configuration
-// Importing these classes is effectively the same as declaring bean methods
-@Import({TracingClientHttpRequestInterceptor.class, TracingHandlerInterceptor.class})
-class TracingConfiguration extends WebMvcConfigurerAdapter {
-    @Value("${mcp.zipkin.url}")
-    String zipkinSenderUrl;
+class TracingConfiguration {
 
-    /**
-     * Configuration for how to send spans to Zipkin
-     */
-    @Bean
-    Sender sender() {
-        return OkHttpSender.create(zipkinSenderUrl);
-    }
+	@Bean
+	HandlerInterceptor serverInterceptor(HttpTracing tracing) {
+		return TracingHandlerInterceptor.create(tracing);
+	}
 
-    /**
-     * Configuration for how to buffer spans into messages for Zipkin
-     */
-    @Bean
-    AsyncReporter<Span> spanReporter() {
-        return AsyncReporter.create(sender());
-    }
+	@Bean
+	ClientHttpRequestInterceptor httpRequestInterceptor(HttpTracing tracing) {
+		return TracingClientHttpRequestInterceptor.create(tracing);
+	}
 
-    /**
-     * Put all of the application-specific tracing priorities in this method
-     * Controls aspects of tracing such as the name that shows up in the UI
-     */
-    @Bean
-    Tracing tracing(@Value("${mcp:spring-tracing}") String serviceName) {
-        return Tracing.newBuilder()
-                .sampler(Sampler.ALWAYS_SAMPLE)
-                .localServiceName(serviceName)
-                .propagationFactory(ExtraFieldPropagation
-                        .newFactory(B3Propagation.FACTORY, "client-id"))
-                // puts trace IDs into logs
-                //.currentTraceContext(ThreadContextCurrentTraceContext.create())
-                // subclassed to enable MDC synchronizing
-                .currentTraceContext(MDCCurrentTraceContext.create())
-                .spanReporter(spanReporter()).build();
-    }
+	@Bean
+	RestTemplateCustomizer clientInterceptor(ClientHttpRequestInterceptor c) {
+		return restTemplate -> restTemplate.getInterceptors().add(c);
+	}
 
-    // decides how to name and tag spans. By default they are named the same as the http method.
-    @Bean
-    HttpTracing httpTracing(Tracing tracing) {
-        return HttpTracing.create(tracing);
-    }
+	/**
+	 * Configuration for how to send spans to Zipkin
+	 */
+	@Bean
+	Sender sender(@Value("${mcp.zipkin.url}") String zipkinSenderUrl) {
+		return OkHttpSender.create(zipkinSenderUrl);
+	}
 
-    @Autowired
-    private TracingHandlerInterceptor serverInterceptor;
+	/**
+	 * Configuration for how to buffer spans into messages for Zipkin
+	 */
+	@Bean
+	AsyncReporter<Span> spanReporter(Sender sender) {
+		return AsyncReporter.create(sender);
+	}
 
-    @Autowired
-    private TracingClientHttpRequestInterceptor clientInterceptor;
+	/**
+	 * Put all of the application-specific tracing priorities in this method
+	 * Controls aspects of tracing such as the name that shows up in the UI
+	 */
+	@Bean
+	Tracing tracing(@Value("${mcp:spring-tracing}") String serviceName,
+	                AsyncReporter<Span> spanAsyncReporter) {
+		return Tracing.newBuilder()
+				.sampler(Sampler.ALWAYS_SAMPLE)
+				.localServiceName(serviceName)
+				.propagationFactory(ExtraFieldPropagation
+						.newFactory(B3Propagation.FACTORY, "client-id"))
+				// puts trace IDs into logs
+				//.currentTraceContext(ThreadContextCurrentTraceContext.create())
+				// subclassed to enable MDC synchronizing
+				.currentTraceContext(MDCCurrentTraceContext.create())
+				.spanReporter(spanAsyncReporter)
+				.build();
+	}
 
-    @Autowired
-    private RestTemplate restTemplate;
+	// decides how to name and tag spans. By default they are named the same as the http method.
+	@Bean
+	HttpTracing httpTracing(Tracing tracing) {
+		return HttpTracing.create(tracing);
+	}
 
-    /**
-     * adds tracing to the application-defined rest template
-     */
-    @PostConstruct
-    public void init() {
-        List<ClientHttpRequestInterceptor> interceptors =
-                new ArrayList<>(restTemplate.getInterceptors());
-        interceptors.add(clientInterceptor);
-        restTemplate.setInterceptors(interceptors);
-    }
 
-    /**
-     * adds tracing to the application-defined web controller
-     */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(serverInterceptor);
-    }
+	@Configuration
+	public static class MyWebConfig extends WebMvcConfigurerAdapter {
+
+		private final HandlerInterceptor serverInterceptor;
+
+		public MyWebConfig(HandlerInterceptor serverInterceptor) {
+			this.serverInterceptor = serverInterceptor;
+		}
+
+		@Override
+		public void addInterceptors(InterceptorRegistry registry) {
+			registry.addInterceptor(this.serverInterceptor);
+		}
+	}
+
+
 }
