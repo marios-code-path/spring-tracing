a676d4aa1475d76a8c059593be36c4cf521806ed
Mario Gray <mario.gray@gmail.com>
2018-04-16 18:55:20 -0700

    wp

--- trace-http-server/src/main/java/mcp/http/TracingRestController.java
+++ trace-http-server/src/main/java/mcp/http/TracingRestController.java
@@ -1,12 +1,18 @@
 package mcp.http;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 @Profile("http-web")
+@Slf4j
 @RestController
 public class TracingRestController {
     private final RestTemplate restTemplate;
@@ -18,7 +24,16 @@ public class TracingRestController {
 
     @GetMapping("/backend")
     public String backend(@RequestHeader("client-id") String clientId) {
+        restTemplate.getForObject("http://localhost:8080/delay", String.class);
         return "Greetings " + clientId;
+    }
+
+    @GetMapping("/delay")
+    public void addDelay() throws Exception{
+        long delay = ThreadLocalRandom.current().nextInt(1, 5);
+        log.info("Goind to Delay for: " + delay + "seconds");
+        TimeUnit.SECONDS.sleep(delay);
+        return;
     }
 
     @GetMapping("/frontend")
